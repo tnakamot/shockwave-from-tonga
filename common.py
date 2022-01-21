9d6ae9281f1ae90b19e2d1253b5d22f88b3149d5
Takashi Nakamoto <nyakamoto@gmail.com>
2022-01-21 03:39:41 -0800

    Refactoring.

---
+++ common.py
@@ -0,0 +1,125 @@
+
+#
+# Python module that defines common constants, functions and classes that are
+# useful for analysing and visualizing the shockwave from Hunga Tonga.
+#
+# Copyright (C) 2022 Takashi Nakamoto
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+import contextlib
+import io
+from datetime import datetime, timedelta, timezone
+
+import cartopy.feature as cfea
+from geopy.distance import geodesic
+from geopy.point import Point
+from matplotlib.figure import Figure
+from matplotlib.font_manager import FontProperties, findfont
+from PIL import Image, ImageDraw, ImageFont
+
+# Japan Standard Time
+TZ_JST = timezone( timedelta( hours = +9 ), name = 'JST' ) # Japan Standard Time
+
+OCEAN_COLOR = '#00FFFF'
+LAND_COLOR  = '#32CD32'
+
+# Coordinate of Hunga Tonga.
+HUNGA_TONGA_COORD   = Point( latitude = -20.536 , longitude = -175.382 )
+
+# Coordinate of antipode of Hunga Tonga.
+ANTIPODE_COORD      = Point( latitude  = - HUNGA_TONGA_COORD.latitude,
+                             longitude = 180 + HUNGA_TONGA_COORD.longitude )
+
+# Earth circumference.
+EARTH_CIRCUMFERENCE = geodesic( HUNGA_TONGA_COORD, ANTIPODE_COORD ) * 2
+
+# Hunga Tonga eruption time estimated from the satellite image of Himawari 8
+#  https://himawari.asia/himawari8-image.htm?sI=D531106&sClC=ffff00&sTA=true&sTAT=TY&sS=6&sNx=3&sNy=2&sL=-169.171875&sT=-426.8125&wW=1920&wH=969&sD=1642219800000
+ERUPTION_TIME       = datetime( 2022, 1, 15, 13, 10, tzinfo = TZ_JST )
+
+
+def fig2img(fig):
+    '''Convert the given matplotlib Figure into Pillow Image.
+
+    Parameters:
+    -----------
+    fig: matplotlib.figure.Figure
+        Figure to convert.
+
+    Returns:
+    --------
+    PIL.Image
+        Conversion result.
+    '''
+    buf = io.BytesIO()
+    fig.savefig( buf, bbox_inches = 'tight', pad_inches = 0 )
+    buf.seek( 0 )
+    return Image.open( buf )
+
+def add_world_map(fig, projection):
+    ax = fig.add_subplot( 1, 1, 1, projection = projection )
+    ax.set_extent( ( -180, 180, -90, 90 ), projection )
+    ax.add_feature( cfea.OCEAN, color = OCEAN_COLOR )
+    ax.add_feature( cfea.LAND,  color = LAND_COLOR )
+    return ax
+
+class AnimationFrame:
+    '''Represents one image in the animation data.'''
+    def __init__(self, image, duration_ms):
+        self.image       = image
+        self.duration_ms = duration_ms
+
+class AnimationData:
+    '''Represents one animation data.'''
+
+    def __init__(self, dump_mode = True):
+        self.frames = []
+        self.cover_frame = None
+        self.dump_mode   = dump_mode
+        
+    def append(self, image, duration_ms, dump_path = None):
+        if isinstance(image, Figure):
+            image = fig2img( image )
+            
+        self.frames.append( AnimationFrame( image, duration_ms ) )
+        if self.dump_mode and dump_path:
+            image.save( dump_path )
+
+    def add_cover(self, text, fontsize = 24, duration_ms = 2000):
+        image_size  = self.frames[0].image.size
+        cover_image = Image.new( 'RGB', image_size, 'black' )
+        cover_draw  = ImageDraw.Draw( cover_image )
+        cover_font  = ImageFont.truetype( findfont( FontProperties( family = ['monospace'] ) ),
+                                          size = fontsize )
+        cover_draw.text( ( image_size[0] / 2, image_size[1] / 2 ),
+                         text,
+                         fill   = 'white',
+                         font   = cover_font,
+                         anchor = 'mm',
+                         align  = 'center' )
+        
+        self.cover_frame = AnimationFrame( cover_image, duration_ms )
+        
+    def save_gif(self, path, loop = True):
+        frames = [self.cover_frame] if self.cover_frame else []
+        frames = frames + self.frames
+
+        frames[0].image.save( path,
+                              append_images = [ frame.image for frame in frames[1:] ],
+                              save_all = True,
+                              duration = [ frame.duration_ms for frame in frames ],
+                              loop = 0 if loop else 1 )
--- wavefront_simulation.py
+++ wavefront_simulation.py
@@ -20,60 +20,33 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
+import os
+import sys
+sys.path.append( os.path.join( os.path.dirname( __file__ ) ) )
+
 import io
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
 import cartopy.crs as ccrs
-import cartopy.feature as cfea
 import matplotlib.pyplot as plt
 import numpy as np
 from geopy.distance import geodesic
-from matplotlib.font_manager import FontProperties, findfont
-from PIL import Image, ImageDraw, ImageFont
+from tqdm import tqdm
 
-OUTPUT_DIR = Path( 'figure_wavefront_simulation' )
-OUTPUT_DIR.mkdir( parents = True, exist_ok = True )
+from common import *
 
-TZ_JST = timezone( timedelta( hours = +9 ), name = 'JST' ) # Japan Standard Time
-GENERATE_ANIMATION_GIF = True
-
-OCEAN_COLOR = '#00FFFF'
-LAND_COLOR  = '#32CD32'
-
-hunga_tonga_coord   = ( -20.536   , -175.382   )
-antipode_coord      = ( - hunga_tonga_coord[0], 180 + hunga_tonga_coord[1] )
-earth_circumference = geodesic( hunga_tonga_coord, antipode_coord ) * 2
-eruption_time       = datetime( 2022, 1, 15, 13, 10, tzinfo = TZ_JST )
-travel_speed_m_s    = 310 # [m/s]
-
-fig = plt.figure( figsize = (10, 5) )
-
-def fig2img(fig):
-    buf = io.BytesIO()
-    fig.savefig( buf, bbox_inches='tight', pad_inches = 0 )
-    buf.seek( 0 )
-    img = Image.open( buf )
-    return img
-
-
-animation_images = []
-
-for minutes_from_eruption in range(0, 72 * 60, 10):
+def draw_wavefront(fig, minutes_from_eruption, travel_speed_m_s):
     time_since_eruption = timedelta( minutes = minutes_from_eruption )
-    date_time = eruption_time + time_since_eruption
+    date_time = ERUPTION_TIME + time_since_eruption
     bearings = np.linspace(-180, 180, 360)
-    distance = geodesic( meters = travel_speed_m_s * ( date_time - eruption_time ).total_seconds() )
-    wavefront_points = [ distance.destination( point = hunga_tonga_coord, bearing = b ) for b in bearings ]
+    distance = geodesic( meters = travel_speed_m_s * ( date_time - ERUPTION_TIME ).total_seconds() )
+    wavefront_points = [ distance.destination( point = HUNGA_TONGA_COORD, bearing = b ) for b in bearings ]
 
     wavefront_latitude_deg  = [ p[0] for p in wavefront_points ]
     wavefront_longitude_deg = [ p[1] for p in wavefront_points ]
 
-    ax = fig.add_subplot( 1, 1, 1,
-                          projection = ccrs.PlateCarree() )
-    ax.set_extent( ( -180, 180, -90, 90 ), ccrs.PlateCarree() )
-    ax.add_feature( cfea.OCEAN, color = OCEAN_COLOR )
-    ax.add_feature( cfea.LAND,  color = LAND_COLOR )
+    ax = add_world_map( fig, ccrs.PlateCarree() )
 
     div_js = np.where( np.abs( np.diff( wavefront_longitude_deg ) ) > 180 )[0] + 1
     div_js = np.append( div_js, len( wavefront_longitude_deg ) )
@@ -85,7 +58,7 @@ for minutes_from_eruption in range(0, 72 * 60, 10):
                  color = 'black' )
         div_j_start = div_j_end
 
-    hours_since_eruption    = int( time_since_eruption.total_seconds() / 3600 )
+    hours_since_eruption   = int( time_since_eruption.total_seconds() / 3600 )
     minutes_since_eruption = int( ( time_since_eruption.total_seconds() % 3600 ) / 60 )
         
     title  = 'Estimated wavefront from Hunga Tonga\n'
@@ -94,38 +67,44 @@ for minutes_from_eruption in range(0, 72 * 60, 10):
     title += f'(Assumed travel speed = {travel_speed_m_s:.1f} m/s)'
     ax.set_title( title )
 
-    if GENERATE_ANIMATION_GIF:
-        print(f'Internally generated the map at {hours_since_eruption:3d}:{minutes_since_eruption:02d} since the eruption.')
-        animation_images.append( fig2img( fig ) )
-    else:
-        fig_output_filename = OUTPUT_DIR / f'wavefront_simulation_{minutes_from_eruption:05d}.png'
-        fig.savefig( fig_output_filename, bbox_inches='tight', pad_inches = 0 )
-        print( f'Generated {fig_output_filename}' )
-        
-    fig.clear()
 
+def main():
+    # Estimated travel speed of shock wave to draw.
+    TRAVEL_SPEED_M_S = 310 # [m/s]
 
-if GENERATE_ANIMATION_GIF:
-    # Generate the cover page.
-    image_size = animation_images[0].size
-    cover_image = Image.new( 'RGB', image_size, 'black' )
-    cover_draw  = ImageDraw.Draw( cover_image )
-    cover_font  = ImageFont.truetype( findfont( FontProperties( family = ['monospace'] ) ),
-                                      size = 24 )
-    cover_text  = 'Estimated wavefront of the shockwave\nfrom Hunga Tonga'
-    cover_draw.text( ( image_size[0] / 2, image_size[1] / 2 ),
-                     cover_text,
-                     fill = 'white',
-                     font = cover_font,
-                     anchor = 'mm',
-                     align = 'center' )
+    OUTPUT_DIR = Path( 'figure_wavefront_simulation' )
+    OUTPUT_DIR.mkdir( parents = True, exist_ok = True )
     
-    # Generate animation GIF.
-    duration = [2000] + [100] * len( animation_images )
+    SIMULATION_INTERVAL_MINUTES = 10
+    SIMULATION_DURATION_MINUTES = 72 * 60
+
+    fig = plt.figure( figsize = (10, 5) )
+    dump_mode = True
+    animation_data = AnimationData( dump_mode = dump_mode )
+    simulation_range = range( 0,
+                              SIMULATION_DURATION_MINUTES + SIMULATION_INTERVAL_MINUTES,
+                              SIMULATION_INTERVAL_MINUTES )
+
+    for minutes_from_eruption in tqdm( simulation_range ):
+        draw_wavefront( fig, minutes_from_eruption, TRAVEL_SPEED_M_S )
+        
+        dump_path = OUTPUT_DIR / f'wavefrom_simulation_{minutes_from_eruption:05d}.png'
+        
+        animation_data.append( fig,
+                               duration_ms = 100,
+                               dump_path = dump_path )
+        if dump_mode:
+            tqdm.write( f'Generated {dump_path}' )
+
+        fig.clear()
+        
+        
+    animation_data.add_cover( text = 'Estimated wavefront of the shockwave\nfrom Hunga Tonga',
+                              fontsize = 24,
+                              duration_ms = 2000 )
     gif_output_filename = OUTPUT_DIR / f'wavefront_simulation.gif'
-    cover_image.save( gif_output_filename,
-                      append_images = animation_images,
-                      save_all = True,
-                      duration = duration,
-                      loop = 0 )
+    animation_data.save_gif( gif_output_filename )
     print( f'Generated {gif_output_filename}' )
+
+if __name__ == "__main__":
+    main()
